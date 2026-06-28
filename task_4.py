0c0d033b02ee2aa2f9497a7a584f838d982bb224
Sean Yang <60932078+s303824@users.noreply.github.com>
2026-06-27 21:42:57 -0400

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,83 @@
+from dash import Dash, html, dcc, Input, Output, callback
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("task_2.csv", header=0, parse_dates=[1], date_format="%Y-%m-%d")
+
+df['sale'] = df['sale'].astype(float)
+df['region'] = df['region'].astype(str)
+
+processed_data = df.groupby('date').agg(
+    daily_sales=('sale', 'sum')
+)
+colors = {
+    'background': '#343830',
+    'north': '#5097B7',
+    'south': '#7EB851',
+    'east': '#B88951',
+    'west': '#B051B8',
+    'all': '#4C5C63',
+    'text': '#FFFFFF'
+}
+
+fig = px.line(processed_data, y="daily_sales", color_discrete_sequence=[colors['all']], title='Daily Sales over Time')
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app = Dash()
+app.layout = html.Div(children=[
+    html.H1(style={'color': colors['text']}, children='Soul Foods'),
+
+    html.Div(style={'color': colors['text']}, children='''
+        Were sales higher before or after the Pink Morsel price increase on the 15th of January, 2021?
+    '''),
+    dcc.RadioItems(
+        id="selection",
+        options=["north", "south", "east", "west", "all"],
+        value="all",
+        labelStyle={
+            'color': colors['text']
+        },
+        inline=True),
+    dcc.Graph(
+        id='graph',
+        figure=fig
+    )
+], style={
+    'backgroundColor': colors['background'],
+    'height': '100vh',
+    'padding': '0px',
+    'margin': '0px'
+})
+
+
+@callback(
+    Output('graph', 'figure'),
+    Input("selection", 'value')
+)
+def select_data(region):
+    if "all" == region:
+        processed = df.groupby('date').agg(
+            daily_sales=('sale', 'sum')
+        )
+    else:
+        filtered_data = df[df['region'] == region]
+        processed = filtered_data.groupby('date').agg(
+            daily_sales=('sale', 'sum')
+        )
+
+    result = px.line(processed, y="daily_sales", color_discrete_sequence=[colors[region]], title='Daily Sales over Time')
+    result.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
+
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
